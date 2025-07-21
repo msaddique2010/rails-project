3c38a0eb4ac4252ea3de44cb8b447658155cce50
Muhammad Saddique <saddiquem2010@gmail.com>
2025-07-21 15:23:10 +0500

    Created authentication using devise and also set saddiquem1010@gmail.com as admin of the page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[ new edit update destroy ]
 
   # GET /products or /products.json
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_21_094106) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_21_101550) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_094106) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
