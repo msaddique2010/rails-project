3efbae4d3bb70fcb2cfdd8a6105ca51b083bbdf1
Muhammad Saddique <saddiquem2010@gmail.com>
2025-07-21 16:54:56 +0500

    Added Cancan authorization that only admin can create product but user can only use index page and show products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = "Access denied: You are not authorized to perform this action."
+    redirect_to(request.referer || root_path)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  validates :name, :description, presence: true
 end
